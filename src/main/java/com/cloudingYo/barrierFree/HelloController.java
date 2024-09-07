3fa4365bb5c37fc7d1139dad9154754f00160031
KIM DONGHO <dpl9753@naver.com>
2024-09-08 01:40:34 +0900

    edit

--- src/main/java/com/cloudingYo/barrierFree/HelloController.java
+++ src/main/java/com/cloudingYo/barrierFree/HelloController.java
@@ -1,5 +1,6 @@
 package com.cloudingYo.barrierFree;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")  // 모든 도메인에서 요청 허용
 public class HelloController {
     @GetMapping("/api/hello")
     public Map<String, String> sayHello() {
