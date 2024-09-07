5f9085ca1701c677bf4ca5bd35ff519659901ca4
KIM DONGHO <dpl9753@naver.com>
2024-09-07 21:20:25 +0900

    front test

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM openjdk:17-jdk
 
 COPY ./build/libs/*SNAPSHOT.jar server.jar
 
+EXPOSE 8080
+
 ENTRYPOINT [ "java", "-jar", "/server.jar" ]
---
+++ src/main/java/com/cloudingYo/barrierFree/HelloController.java
@@ -0,0 +1,17 @@
+package com.cloudingYo.barrierFree;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public Map<String, String> sayHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "연결 테스트 중입니다!");
+        return response;
+    }
+}
