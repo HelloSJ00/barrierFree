220dfb0510c1706b174224f24f7df5ac9792f9f9
오승준 <skku03062@g.skku.edu>
2024-10-12 06:33:59 +0900

    fix WebConfig

--- src/main/java/com/cloudingYo/barrierFree/common/WebConfig.java
+++ src/main/java/com/cloudingYo/barrierFree/common/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**") // 특정 경로에 대해 CORS 허용
+        registry.addMapping("/**") // 특정 경로에 대해 CORS 허용
                 .allowedOrigins("http://localhost:3000") // 허용할 도메인 (Next.js 도메인)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
