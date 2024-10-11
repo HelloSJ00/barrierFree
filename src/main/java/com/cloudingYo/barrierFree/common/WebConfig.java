971463d8527bb618a31a67b6fc4a00a3801c82c2
오승준 <skku03062@g.skku.edu>
2024-10-12 06:44:12 +0900

    cors 해결됐나?

--- src/main/java/com/cloudingYo/barrierFree/common/WebConfig.java
+++ src/main/java/com/cloudingYo/barrierFree/common/WebConfig.java
@@ -1,6 +1,7 @@
 package com.cloudingYo.barrierFree.common;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,9 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
+        WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**") // 특정 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000") // 허용할 도메인 (Next.js 도메인)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
+                .allowedOrigins("*") // 허용할 도메인 (Next.js 도메인)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()) // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true) // 쿠키 등 자격 증명 허용 여부
                 .maxAge(3600); // 설정된 CORS의 유효 시간 (초)
