154f48f697327828762d8033047d1361fd2687ab
오승준 <skku03062@g.skku.edu>
2024-10-14 18:42:29 +0900

    리뷰데이터를 세션으로 관리하게끔 함

--- src/main/java/com/cloudingYo/barrierFree/review/controller/ReviewController.java
+++ src/main/java/com/cloudingYo/barrierFree/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package com.cloudingYo.barrierFree.review.controller;
 
 import com.cloudingYo.barrierFree.common.entity.ApiResponse;
 import com.cloudingYo.barrierFree.review.dto.ReviewDTO;
+import com.cloudingYo.barrierFree.review.dto.ReviewResponseDTO;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 public interface ReviewController {
-    ResponseEntity<ApiResponse<?>> getReview(Long placeId, Long userId);
-    ResponseEntity<ApiResponse<?>> getReviews(Long placeId);
-    ResponseEntity<ApiResponse<?>> registerReview(@RequestBody ReviewDTO reviewDTO, HttpSession session);
-    ResponseEntity<ApiResponse<?>> updateReview(@RequestBody ReviewDTO reviewDTO, HttpSession session);
-    ResponseEntity<ApiResponse<?>> deleteReview(@RequestBody ReviewDTO reviewDTO, HttpSession session);
+    ResponseEntity<ReviewResponseDTO<?>> getReview(Long placeId, Long userId);
+    ResponseEntity<ReviewResponseDTO<?>> getReviews(Long placeId);
+    ResponseEntity<ReviewResponseDTO<?>> registerReview(@RequestBody ReviewDTO reviewDTO, HttpSession session);
+    ResponseEntity<ReviewResponseDTO<?>> updateReview(@RequestBody ReviewDTO reviewDTO, HttpSession session);
+    ResponseEntity<ReviewResponseDTO<?>> deleteReview(@RequestBody ReviewDTO reviewDTO, HttpSession session);
 }
--- src/main/java/com/cloudingYo/barrierFree/review/dto/ReviewResponseDTO.java
+++ src/main/java/com/cloudingYo/barrierFree/review/dto/ReviewResponseDTO.java
@@ -8,9 +8,38 @@ import org.bson.types.ObjectId;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReviewResponseDTO {
-        private Long placeId;
-        private Long userId;
-        private int rating;
-        private String content;
+public class ReviewResponseDTO<T> {
+        private int status;
+        private String message;
+        private T data;  // data 필드에 제네릭을 사용하여 다양한 타입의 데이터를 받도록 설정
+
+        public static <T> ReviewResponseDTO<T> fail(String s) {
+                return ReviewResponseDTO.<T>builder()
+                        .status(400)
+                        .message(s)
+                        .build();
+        }
+
+        public static <T> ReviewResponseDTO<T> success(String s) {
+                return ReviewResponseDTO.<T>builder()
+                        .status(200)
+                        .message(s)
+                        .build();
+        }
+
+        public static <T> ReviewResponseDTO<T> success(String s, T data) {
+                return ReviewResponseDTO.<T>builder()
+                        .status(200)
+                        .message(s)
+                        .data(data)
+                        .build();
+        }
+
+        public static <T> ReviewResponseDTO<T> fail(String s, T data) {
+                return ReviewResponseDTO.<T>builder()
+                        .status(400)
+                        .message(s)
+                        .data(data)
+                        .build();
+        }
 }
