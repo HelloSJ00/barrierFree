a4499f2a726fb51faa6b4edac0145559661663f5
오승준 <skku03062@g.skku.edu>
2024-10-31 00:30:19 +0900

    몽고디비 설정 수정

--- src/main/java/com/cloudingYo/barrierFree/common/mongodb/MongoConfig.java
+++ src/main/java/com/cloudingYo/barrierFree/common/mongodb/MongoConfig.java
@@ -5,14 +5,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 public class MongoConfig {
 
     @Value("${spring.data.mongodb.uri}")
-    String MongoURI;
+    private String mongoURI;
+
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(MongoClients.create(), "prod_db");
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoURI));
     }
+
+
 }
