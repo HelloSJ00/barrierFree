0fa2cac0590dc4f8afb6bc249b3effd18a105b3b
오승준 <skku03062@g.skku.edu>
2025-01-05 18:54:53 +0900

    UserRepository 대역에 로직 추가 Fake 대역으로 수정

--- src/test/java/com/cloudingYo/barrierFree/user/DummyPasswordEncoder.java
+++ src/test/java/com/cloudingYo/barrierFree/user/DummyPasswordEncoder.java
@@ -1,2 +1,20 @@
-package com.cloudingYo.barrierFree.user;public class DummyPasswordEncoder {
+package com.cloudingYo.barrierFree.user;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class DummyPasswordEncoder implements PasswordEncoder {
+    @Override
+    public boolean upgradeEncoding(String encodedPassword) {
+        return PasswordEncoder.super.upgradeEncoding(encodedPassword);
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return null;
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return false;
+    }
 }
--- src/test/java/com/cloudingYo/barrierFree/user/service/UserServiceImplTest.java
+++ src/test/java/com/cloudingYo/barrierFree/user/service/UserServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.cloudingYo.barrierFree.user.service;
 
+import com.cloudingYo.barrierFree.user.DummyPasswordEncoder;
 import com.cloudingYo.barrierFree.user.DummyUserRepositoryImpl;
+import com.cloudingYo.barrierFree.user.FakeUserRepositoryImpl;
 import com.cloudingYo.barrierFree.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,16 +13,46 @@ import org.springframework.test.annotation.Commit;
 
 @SpringBootTest
 class UserServiceImplTest {
-    @Autowired
-    private PasswordEncoder passwordEncoder; // 스프링 컨텍스트에서 주입받음
-
     private UserService userService;
-
     @BeforeEach
     void 리포지토리는_더미_대역으로() {
-        userService = new UserServiceImpl(new DummyUserRepositoryImpl(), passwordEncoder);
+        userService = new UserServiceImpl(new FakeUserRepositoryImpl(), new DummyPasswordEncoder());
+    }
+    /*
+     * 테스트 항목
+     * // 기본
+        UserDTO findUser(String email);
+
+        // 회원가입
+        boolean isEmailExists(String email);
+        void registerUser(UserDTO userDTO);
+
+        //회원정보 수정
+        boolean updateUser(String email,String updateUsername);
+
+        //탈퇴
+        boolean deleteUser(UserDTO userDTO);
+     */
+
+
+    /*
+    UserDTO findUser(String email);
+     */
+    @Test
+    void 이메일로_유저를_찾을_수_있다(){
+        //givrn
+
+        //when
+
+        //then
     }
 
+    @Test
+    void 없는_메일로_유저_조회시_오류(){
+        //givrn
 
+        //when
 
+        //then
+    }
 }
