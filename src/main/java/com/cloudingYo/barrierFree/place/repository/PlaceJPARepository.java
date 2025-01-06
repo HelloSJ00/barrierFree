09ff78382b44916fc4f758494805279803cf4656
오승준 <skku03062@g.skku.edu>
2025-01-06 18:19:31 +0900

    장소 좌표 조회시 jpql로 조회로 수정

--- src/main/java/com/cloudingYo/barrierFree/place/repository/PlaceJPARepository.java
+++ src/main/java/com/cloudingYo/barrierFree/place/repository/PlaceJPARepository.java
@@ -3,10 +3,15 @@ package com.cloudingYo.barrierFree.place.repository;
 import com.cloudingYo.barrierFree.place.dto.PlaceCoordinateDTO;
 import com.cloudingYo.barrierFree.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PlaceJPARepository extends JpaRepository<Place, Long> {
-    Optional<PlaceCoordinateDTO> findCoordinateByPlaceKey(int placeKey);
+
+    @Query("SELECT p.placeKey,p.latitude,p.longitude FROM Place p WHERE p.placeKey = :placeKey")
+    Optional<PlaceCoordinateDTO> findCoordinateByPlaceKey(@Param("placeKey") int placeKey);
+
     Optional<Place> findByPlaceKey(int placeKey);
 }
